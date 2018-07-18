d94c07ba1bc32de948e8630c3629ff285e0a63cc
breab <brea.borlas@gmail.com>
2018-07-18 16:48:04 -0700

    Add first chart into History

--- dijibu-react/dijibu/src/History.js
+++ dijibu-react/dijibu/src/History.js
@@ -2,29 +2,33 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 
 function History(props) {
+  let dataValues = {
+    values: [14.3, 14.3, 14.3, 14.3, 14.3, 14.3, 14.3],
+    labels: ["Happy", "Sad", "Anxious", "Sad", "Productive", "Sad", "Happy"],
+    type: 'pie',
+    marker: {
+      colors: ["#F6D1B9", "#092E46", "#D7D7D7", "#092E46", "#738678", "#092E46", "#F6D1B9"]
+    },
+    sort: false,
+    textinfo: 'none'
+  }
+  let layoutValues = {
+    height: 500,
+    width: 600,
+    margin: {
+      l: 20,
+      r: 20,
+      b: 20,
+      t: 20}
+  }
+
     return (
       <div>
         <Plot
-          data={
-          {
-            values: [14.3, 14.3, 14.3, 14.3, 14.3, 14.3, 14.3],
-            labels: ["Happy", "Sad", "Anxious", "Sad", "Productive", "Sad", "Happy"],
-            type: "pie",
-            marker: {
-              colors: ["#F6D1B9", "#092E46", "#D7D7D7", "#092E46", "#738678", "#092E46", "#F6D1B9"]
-            },
-            sort: false,
-            textinfo: "none"
-          }
-        }
-        layout={ {
-        height: 500,
-        width: 600,
-        margin: {
-          l: 20,
-          r: 20,
-          b: 20,
-          t: 20} } }
+          data={[
+            dataValues
+          ]}
+        layout={ layoutValues }
           />
         <h1>{props.historyName}</h1>
       </div>
