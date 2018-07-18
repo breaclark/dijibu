044f6af2f45260b468ba8af9bdba741ec391c39b
breab <brea.borlas@gmail.com>
2018-07-18 16:30:10 -0700

    Attempt to add one plotly chart

--- dijibu-react/dijibu/src/HistoriesMain.js
+++ dijibu-react/dijibu/src/HistoriesMain.js
@@ -5,10 +5,10 @@ function HistoriesMain() {
     return (
       <div>
         <h1>Tracker Histories</h1>
-        <History />
-        <History />
-        <History />
-        <History />
+        <History historyName="Tracker 1"/>
+        <History historyName="Tracker 2"/>
+        <History historyName="Tracker 3"/>
+        <History historyName="Tracker 4"/>
       </div>
     );
 }
--- dijibu-react/dijibu/src/History.js
+++ dijibu-react/dijibu/src/History.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 
-function History() {
+function History(props) {
     return (
       <div>
-        <h1>History</h1>
+        <Plot
+          data={
+          {
+            values: [14.3, 14.3, 14.3, 14.3, 14.3, 14.3, 14.3],
+            labels: ["Happy", "Sad", "Anxious", "Sad", "Productive", "Sad", "Happy"],
+            type: "pie",
+            marker: {
+              colors: ["#F6D1B9", "#092E46", "#D7D7D7", "#092E46", "#738678", "#092E46", "#F6D1B9"]
+            },
+            sort: false,
+            textinfo: "none"
+          }
+        }
+        layout={ {
+        height: 500,
+        width: 600,
+        margin: {
+          l: 20,
+          r: 20,
+          b: 20,
+          t: 20} } }
+          />
+        <h1>{props.historyName}</h1>
       </div>
     );
 }
