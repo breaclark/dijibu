f38d666c89d4e2425a70600c1d42fcc7f7c0d9f7
breab <brea.borlas@gmail.com>
2018-07-19 13:30:19 -0700

    Add test wordcloud to History component

--- dijibu-react/dijibu/src/History.js
+++ dijibu-react/dijibu/src/History.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import WordCloud from 'react-d3-cloud';
 
 function History(props) {
+  const data = [
+  { text: "word", value: 25 },
+  { text: "quick", value: 43 },
+  { text: "brown", value: 42 },
+  { text: "fox", value: 13 },
+  { text: "jumped", value: 16 },
+  { text: "over", value: 14 },
+  { text: "the", value: 28 },
+  { text: "lazy", value: 33 }
+  ];
+
+  const fontSizeMapper = word => Math.log2(word.value) * 5;
 
     return (
       <div>
@@ -11,6 +24,10 @@ function History(props) {
           ]}
         layout={ props.layoutValues }
           />
+        <WordCloud
+          data={data}
+          fontSizeMapper={fontSizeMapper}
+        />
         <h1>{props.historyName}</h1>
       </div>
     );
