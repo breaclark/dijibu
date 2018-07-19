d46750005d1f8855be29b884ce08a84ac2a38ebe
breab <brea.borlas@gmail.com>
2018-07-19 10:24:14 -0700

    Add plotly charts to histories main

--- dijibu-react/dijibu/src/App.js
+++ dijibu-react/dijibu/src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       date: new Date(),
-      dataValues: {
+      dataValues1: {
         values: [14.3, 14.3, 14.3, 14.3, 14.3, 14.3, 14.3],
         labels: ["Happy", "Sad", "Anxious", "Sad", "Productive", "Sad", "Happy"],
         type: 'pie',
@@ -27,7 +27,7 @@ class App extends React.Component {
         sort: false,
         textinfo: 'none'
       },
-      layoutValues: {
+      layoutValues1: {
         height: 500,
         width: 600,
         margin: {
@@ -35,6 +35,65 @@ class App extends React.Component {
           r: 20,
           b: 20,
           t: 20}
+      },
+      dataValues3: {
+        type: "scatterpolar",
+        r: [33, 80, 98, 8, 120, 57, 139, 33],
+        theta: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        fill: "toself",
+        connectgaps: true,
+        fillcolor: "rgba(246, 209, 185, 0.5)",
+        line: {
+          color: "#DA6257"
+        }
+      },
+      layoutValues3: {
+        polar: {
+          radialaxis: {
+            color: "#DA6257",
+            visible: true,
+            angle: 90,
+            linecolor: "#D7D7D7",
+            gridcolor: "#D7D7D7",
+            nticks: 3,
+            tickangle: 90
+          },
+          angularaxis: {
+            color: "#D7D7D7",
+            direction: "clockwise"
+          }
+        },
+        height: 550,
+        width: 520,
+        margin: {
+          l: 60,
+          r: 40,
+          b: 20,
+          t: 20
+        },
+        showlegend: false
+      },
+      dataValues5:{
+        values: [14.285, 14.285, 14.285, 14.285, 14.285, 14.285, 14.285],
+        labels: ["Sunday", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday"],
+        type: "pie",
+        marker: {
+          colors: ["#F6D1B9", "#FFFFFF", "#F6D1B9", "#F6D1B9", "#F6D1B9", "#FFFFFF", "#F6D1B9"]
+        },
+        sort: false,
+        hole: 0.85,
+        textinfo: "none"
+      },
+      layoutValues5: {
+        height: 550,
+        width: 600,
+        margin: {
+          l: 20,
+          r: 20,
+          b: 20,
+          t: 20
+        },
+        showlegend: false
       }
     };
   }
@@ -47,8 +106,12 @@ class App extends React.Component {
           <Route exact path='/signIn' render={()=><SignIn />}/>
           <Route exact path='/' render={()=><TrackersMain date={this.state.date}/>}/>
           <Route path='/calendar' render={()=><Calendar date={this.state.date}/> }/>
-          <Route path='/histories' render={()=><HistoriesMain dataValues={this.state.dataValues}
-          layoutValues={this.state.layoutValues} /> }/>
+          <Route path='/histories' render={()=><HistoriesMain dataValues1={this.state.dataValues1}
+          layoutValues1={this.state.layoutValues1}
+          dataValues3={this.state.dataValues3}
+          layoutValues3={this.state.layoutValues3}
+          dataValues5={this.state.dataValues5}
+          layoutValues5={this.state.layoutValues5} /> }/>
           <Route path='/edit' render={()=><EditTrackers /> }/>
           <Route path='/new' render={()=><NewTracker /> }/>
           <Route path='/delete' render={()=><DeleteTracker /> }/>
--- dijibu-react/dijibu/src/HistoriesMain.js
+++ dijibu-react/dijibu/src/HistoriesMain.js
@@ -6,12 +6,19 @@ function HistoriesMain(props) {
       <div>
         <h1>Tracker Histories</h1>
         <History
-        dataValues={props.dataValues}
-        layoutValues={props.layoutValues}
+        dataValues={props.dataValues1}
+        layoutValues={props.layoutValues1}
         historyName="Tracker 1"/>
         <History historyName="Tracker 2"/>
-        <History historyName="Tracker 3"/>
+        <History
+        dataValues={props.dataValues3}
+        layoutValues={props.layoutValues3}
+        historyName="Tracker 3"/>
         <History historyName="Tracker 4"/>
+        <History
+        dataValues={props.dataValues5}
+        layoutValues={props.layoutValues5}
+        historyName="Tracker 5"/>
       </div>
     );
 }
