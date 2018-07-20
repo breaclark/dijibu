4711faf15cc9f198f327a3393e38e2d0748c585d
breab <brea.borlas@gmail.com>
2018-07-19 20:20:39 -0700

    Attempt to put in circular heat chart

--- dijibu-react/dijibu/src/History.js
+++ dijibu-react/dijibu/src/History.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import WordCloud from 'react-d3-cloud';
+import { select } from 'd3-selection';
+import { circularHeatChart } from './circularHeatChart.js';
 
 function History(props) {
   const data = [
@@ -16,6 +18,22 @@ function History(props) {
 
   const fontSizeMapper = word => Math.log2(word.value) * 5;
 
+  let chartArea;
+  let chart = circularHeatChart()
+      .range(["#F6D1B9", "#092E46"])
+      .innerRadius(20);
+
+  let data4 = [];
+  for(let i=0; i<168; i++) data4[i] = Math.random();
+  select(chartArea)
+      .selectAll('svg')
+      .data([data4])
+      .enter()
+      .append('svg')
+      .attr("width", 600)
+      .attr("height", 600)
+      .call(chart);
+
     return (
       <div>
         <Plot
@@ -28,6 +46,9 @@ function History(props) {
           data={data}
           fontSizeMapper={fontSizeMapper}
         />
+      <svg ref={chartArea => this.chartArea = chartArea}
+      width={600} height={600}>
+      </svg>
         <h1>{props.historyName}</h1>
       </div>
     );
