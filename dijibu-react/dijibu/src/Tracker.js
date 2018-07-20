e77f0a64e4e491ea9f5593d8d3d1987a35fb88b6
breab <brea.borlas@gmail.com>
2018-07-20 11:56:52 -0700

    Put firebase data through to Tracker Main so that correct trackers are shown for requested date

--- dijibu-react/dijibu/src/Tracker.js
+++ dijibu-react/dijibu/src/Tracker.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Tracker(props) {
     return (
       <div>
-        <h1>{props.name}</h1>
-        <p>{props.options}</p>
-        <p>{props.value}</p>
+        <h1>{props.tracker.name}</h1>
+        <p>{props.tracker.options}</p>
+        <p>{props.tracker.value}</p>
       </div>
     );
 }
--- dijibu-react/dijibu/src/TrackersMain.js
+++ dijibu-react/dijibu/src/TrackersMain.js
@@ -3,20 +3,29 @@ import Tracker from './Tracker';
 import dateFns from "date-fns";
 
 function TrackersMain(props) {
+
+    let requestedDate = dateFns.format(props.date, 'MMMM D, YYYY');
+    let requestedDatesTrackers;
+    for(let i=0; i<props.dates.length; i++) {
+      if (dateFns.format(props.dates[i].date, 'MMMM D, YYYY') === requestedDate){
+        requestedDatesTrackers = props.dates[i].trackers;
+      }
+    }
     return (
       <div>
-        <h1>{dateFns.format(props.date, 'MMMM D, YYYY')}</h1>
+        <h1>{requestedDate}</h1>
+        <p>{}</p>
+        {Object.keys(requestedDatesTrackers).map(function(trackerId){
+          const tracker = requestedDatesTrackers[trackerId];
+          return <Tracker
+            trackerId={trackerId}
+            key={trackerId}
+            tracker={tracker} />
+        })}
         {/*// Should be able to get the date from the calendar as well
         // Then render and send appropriate data for each date
         // Only let trackers be changed if the date is today (later)
-        // Get info passed down from top, then translate here into trackers
-        // {Object.keys(trackers).map(function(trackerId){
-        //   const tracker =trackers[trackerId];
-        //   return <Tracker
-        //     historyId={trackerId}
-        //     key={trackerId}
-        //     tracker={tracker} />
-        // })}*/}
+        // Get info passed down from top, then translate here into trackers*/}
       </div>
     );
 }
