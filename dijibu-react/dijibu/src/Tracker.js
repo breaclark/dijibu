8b7b94261f86e9f0e983e4dc60e1507f3c4f548a
breab <brea.borlas@gmail.com>
2018-07-20 12:10:36 -0700

    Add logic for different trackers to Tracker

--- dijibu-react/dijibu/src/Tracker.js
+++ dijibu-react/dijibu/src/Tracker.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
 function Tracker(props) {
+  if (props.tracker.type === 'pie') {
+    return (
+      <div className="multi-tracker tracker-tile">
+          <p>{props.tracker.name} - {props.tracker.value} <br />
+          (tap to change)</p>
+        // add in a function to change this
+        // might want to make these into different components?
+      </div>
+    );
+  } else if (props.tracker.type === 'wordcloud') {
     return (
       <div>
+      </div>
+    );
+  } else if (props.tracker.type === 'heat') {
+    return (
+      <div>
+      </div>
+    );
+  } else if (props.tracker.type === 'boolean') {
+    return (
+      <div>
+      </div>
+    );
+  } else if (props.tracker.type === 'count') {
+    return (
+      <div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+      </div>
+    );
+  }
+    return (
+      <div>
+
         <h1>{props.tracker.name}</h1>
         <p>{props.tracker.options}</p>
         <p>{props.tracker.value}</p>
