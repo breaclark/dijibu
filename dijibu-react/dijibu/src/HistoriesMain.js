cb24e6145feb9be636f6b0540449df41286dcd06
Guest User <Guest@Epicodus-8C.local>
2018-07-23 10:44:14 -0700

    Add working boolean chart to histories main

--- dijibu-react/dijibu/src/HistoriesMain.js
+++ dijibu-react/dijibu/src/HistoriesMain.js
@@ -65,12 +65,81 @@ function HistoriesMain(props) {
     );
   }
 
+  function parseBooleanData(name) {
+    let labels = [];
+    let colors = [];
+    for(let i=props.dates.length - 1; i>=props.dates.length - 8; i--) {
+      if(i <  0) {
+        break;
+      } else {
+        let possibleElement = props.dates[i].trackers.find(function(element) {
+          if (element.type === "boolean" && element.name === name) {
+            return element;
+          }
+        }).value;
+        if (typeof possibleElement !== "undefined") {
+          let colorResult = "#FFFFFF";
+          if (possibleElement) {
+            colorResult = "#F6D1B9";
+          }
+          labels.push(props.dates[i].date);
+          colors.push(colorResult);
+        }
+      }
+    }
+    return {
+      labels: labels,
+      colors: colors
+    }
+  }
+
+  function makeBooleanChart(name) {
+    let parse = parseBooleanData(name);
+    let labels = parse.labels;
+    let colors = parse.colors;
+    return(
+      { type: "boolean",
+        name: "Exercise",
+        dataValues: [{
+          values: [14.285, 14.285, 14.285, 14.285, 14.285, 14.285, 14.285],
+          labels: labels,
+          type: "pie",
+          marker: {
+            colors: colors
+          },
+          sort: false,
+          hole: 0.85,
+          textinfo: "none"
+        }],
+        layout: {
+          height: 550,
+          width: 600,
+          margin: {
+            l: 20,
+            r: 20,
+            b: 20,
+            t: 20
+          },
+          showlegend: false
+        }
+      }
+    );
+  }
+
   function makeHistories() {
     let histories = [];
     let lastDay = props.dates[props.dates.length -1].trackers;
     for(let i=0; i< lastDay.length; i++) {
-      if(lastDay[i].type === "pie") {
+      if (lastDay[i].type === "pie") {
         histories.push(makePieChart(lastDay[i].name));
+      } else if (lastDay[i].type === "wordcloud") {
+
+      } else if (lastDay[i].type === "heat") {
+
+      } else if (lastDay[i].type === "boolean") {
+        histories.push(makeBooleanChart(lastDay[i].name));
+      } else if (lastDay[i].type === "count") {
+
       }
     }
     return histories;
